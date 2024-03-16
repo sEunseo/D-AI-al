25d04f2bece22402239238a9e4842dcd5dfe0cf4
Mineru <68109760+mineruking@users.noreply.github.com>
2024-03-16 14:53:25 +0900

    1.0.2 - permission added

--- app/src/main/java/com/zerotritwo/d_ai_ler/MainActivity.kt
+++ app/src/main/java/com/zerotritwo/d_ai_ler/MainActivity.kt
@@ -1,46 +1,55 @@
 package com.zerotritwo.d_ai_ler
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import android.telecom.TelecomManager
+import android.widget.Toast
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.zerotritwo.d_ai_ler.ui.theme.DailerTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val REQUEST_CODE_SET_DEFAULT_DIALER = 123
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            DailerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
+        checkDefaultDialer()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            REQUEST_CODE_SET_DEFAULT_DIALER -> checkSetDefaultDialerResult(resultCode)
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+private fun checkDefaultDialer() {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) return
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DailerTheme {
-        Greeting("Android")
+    val telecomManager = getSystemService(TELECOM_SERVICE) as TelecomManager
+        val isAlreadyDefaultDialer = packageName == telecomManager.defaultDialerPackage
+        if (isAlreadyDefaultDialer) return
+
+        val intent = Intent(TelecomManager.ACTION_CHANGE_DEFAULT_DIALER)
+            .putExtra(TelecomManager.EXTRA_CHANGE_DEFAULT_DIALER_PACKAGE_NAME, packageName)
+        startActivityForResult(intent, REQUEST_CODE_SET_DEFAULT_DIALER)
+    }
+
+    private fun checkSetDefaultDialerResult(resultCode: Int) {
+        val message = when (resultCode) {
+            RESULT_OK       -> "User accepted request to become default dialer"
+            RESULT_CANCELED -> "User declined request to become default dialer"
+            else            -> "Unexpected result code $resultCode"
+        }
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
