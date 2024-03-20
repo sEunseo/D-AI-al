5319ffc756cd9ba090f9910f0d868a4492149371
anyubinn <anyubin0724@naver.com>
2024-03-20 13:47:11 +0900

    1.0.4 - some error exists in CallActivity.kt

--- app/src/main/java/com/zerotritwo/d_ai_ler/CallService.kt
+++ app/src/main/java/com/zerotritwo/d_ai_ler/CallService.kt
@@ -1,32 +1,35 @@
 package com.zerotritwo.d_ai_ler
-<<<<<<< HEAD
+
+import android.content.Intent
 import android.telecom.Call
 import android.telecom.InCallService
-//ADD
+import android.util.Log
 
 class CallService : InCallService() {
 
-    //ADD
+    companion object {
+        private const val LOG_TAG = "CallService"
+    }
 
     override fun onCallAdded(call: Call) {
         super.onCallAdded(call)
+        Log.i(LOG_TAG, "onCallAdded: $call")
         call.registerCallback(callCallback)
+        startActivity(Intent(this, CallActivity::class.java))
+        CallManager.updateCall(call)
     }
 
     override fun onCallRemoved(call: Call) {
         super.onCallRemoved(call)
+        Log.i(LOG_TAG, "onCallRemoved: $call")
         call.unregisterCallback(callCallback)
+        CallManager.updateCall(null)
     }
 
     private val callCallback = object : Call.Callback() {
         override fun onStateChanged(call: Call, state: Int) {
-            super.onStateChanged(call, state)
+            Log.i(LOG_TAG, "Call.Callback onStateChanged: $call, state: $state")
+            CallManager.updateCall(call)
         }
     }
 }
-=======
-import android.telecom.InCallService
-//ADD
-
-class CallService : InCallService()
->>>>>>> 25d04f2bece22402239238a9e4842dcd5dfe0cf4
